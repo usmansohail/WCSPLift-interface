ef29892692403abccf12c5706b05521441c8aeba
usmansohail <msohail@usc.edu>
2019-01-31 17:46:59 -0800

    setup .sh file to set environment variables for gurobi

---
+++ setup.sh
@@ -0,0 +1,8 @@
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"  # Remove this line if on MacOS
+export LIBRARY_PATH="${LIBRARY_PATH}:${GUROBI_HOME}/lib"
+export INCLUDE="${INCLUDE}:${GUROBI_HOME}/include"
+export C_INCLUDE_PATH="${C_INCLUDE_PATH}:${GUROBI_HOME}/include"
+export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH}:${GUROBI_HOME}/include"
+
+ls
